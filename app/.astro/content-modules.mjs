c684ad691999a8297c5b33a6d09db2e32225bd40
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2025-10-26 00:08:39 -0700

    chore: fixes docker compose exec; bun test exec

--- app/.astro/content-modules.mjs
+++ app/.astro/content-modules.mjs
@@ -2,8 +2,8 @@
 export default new Map([
 ["src/content/docs/codelab-migrate.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcodelab-migrate.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/codelab-new-project.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcodelab-new-project.mdx&astroContentModuleFlag=true")],
+["src/content/docs/introduction.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduction.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/api-reference.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi-reference.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/setup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsetup.mdx&astroContentModuleFlag=true")],
-["src/content/docs/introduction.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduction.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/workspaces.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fworkspaces.mdx&astroContentModuleFlag=true")]]);
 		
